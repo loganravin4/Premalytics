13aab0733a46b574183fad72fe35f120ae03c887
Logan Ravinuthala <loganravin4@gmail.com>
2025-09-11 10:27:32 -0400

    first commit, repository structure is all setup

---
+++ backend/src/main/java/com/premalytics/PremalyticsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.premalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PremalyticsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PremalyticsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/premalytics/PremalyticsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PremalyticsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
